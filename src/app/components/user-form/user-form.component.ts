ea190d4ef286c64bd3754c5596e7b8860a28a1fd
Gorosss <jongorostegui@gmail.com>
2025-03-11 16:42:14 +0100

    user card ccs header + header css form + form css

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
+  usuarioForm: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.usuarioForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      imagen: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.usuarioForm.valid) {
+      console.log('Formulario enviado:', this.usuarioForm.value);
+      alert('Usuario registrado con éxito');
+      this.usuarioForm.reset();
+      this.submitted = false;
+    }
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { UserService } from '../../services/user.service';
 import { IUser } from '../../interfaces/iuser.interfaces';
 import { IResponse } from '../../interfaces/iresponse.interfaces';
 import { UserCardComponent } from "../../components/user-card/user-card.component";
+import { UserFormComponent } from '../../components/user-form/user-form.component';
 
 @Component({
   selector: 'app-home',
-  imports: [UserCardComponent],
+  imports: [UserCardComponent, UserFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
